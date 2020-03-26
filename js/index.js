631eaab59b20b71847a24ad104708eefc8ed1c11
hieu <phanhieu000248@gmail.com>
2020-03-26 07:11:59 +0700

    add feature

--- js/index.js
+++ js/index.js
@@ -1,80 +1,36 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.9;
-setInterval(drawClock, 1000);
-
-function drawClock() {
-   drawFace(ctx, radius);
-   drawNumbers(ctx, radius);
-   drawTime(ctx, radius);
-}
-
-function drawFace(ctx, radius) {
-   var grad;
-   ctx.beginPath();
-   ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-   ctx.fillStyle = "white";
-   ctx.fill();
-   grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
-   grad.addColorStop(0, '#40e0d0');
-   grad.addColorStop(0.5, "white");
-   grad.addColorStop(1, '#40e0d0');
-   ctx.strokeStyle = grad;
-   ctx.lineWidth = radius * 0.1;
-   ctx.stroke();
-   ctx.beginPath();
-   ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
-   ctx.fillStyle = "#40e0d0";
-   ctx.fill();
-}
-
-function drawNumbers(ctx, radius) {
-   var ang;
-   var num;
-   ctx.font = radius * 0.15 + "px arial";
-   ctx.textBaseline = "middle";
-   ctx.textAlign = "center";
-   for (num = 1; num < 13; num++) {
-      ang = (num * Math.PI) / 6;
-      ctx.rotate(ang);
-      ctx.translate(0, -radius * 0.85);
-      ctx.rotate(-ang);
-      ctx.fillText(num.toString(), 0, 0);
-      ctx.rotate(ang);
-      ctx.translate(0, radius * 0.85);
-      ctx.rotate(-ang);
+$(function () {
+   $('.chart').easyPieChart({
+      barColor: '#17d3e6',
+      scaleColor: false,
+      lineWidth: 10,
+      trackColor: '#333333'
+   });
+});
+function status(event, name) {
+   let project_content = $('.project_content');
+   for (let i = 0; i < project_content.length; i++) {
+      project_content[i].style.display = 'none';
    }
+   let btn = $('.btn');
+   for (let i = 0; i < btn.length; i++) {
+      btn[i].className = btn[i].className.replace('active', '');
+   }
+   document.getElementById(name).style.display = 'block';
+   event.currentTarget.className += ' active';
 }
+document.getElementById("defaultOpen").click();
 
-function drawTime(ctx, radius) {
-   var now = new Date();
-   var hour = now.getHours();
-   var minute = now.getMinutes();
-   var second = now.getSeconds();
-   //hour
-   hour = hour % 12;
-   hour =
-      (hour * Math.PI) / 6 +
-      (minute * Math.PI) / (6 * 60) +
-      (second * Math.PI) / (360 * 60);
-   drawHand(ctx, hour, radius * 0.5, radius * 0.07);
-   //minute
-   minute = (minute * Math.PI) / 30 + (second * Math.PI) / (30 * 60);
-   drawHand(ctx, minute, radius * 0.8, radius * 0.07);
-   // second
-   second = (second * Math.PI) / 30;
-   drawHand(ctx, second, radius * 0.9, radius * 0.02);
-}
-
-function drawHand(ctx, pos, length, width) {
-   ctx.beginPath();
-   ctx.lineWidth = width;
-   ctx.lineCap = "round";
-   ctx.moveTo(0, 0);
-   ctx.rotate(pos);
-   ctx.lineTo(0, -length);
-   ctx.stroke();
-   ctx.rotate(-pos);
-}
+let link = $('.link');
+for (let j = 0; j < link.length; j++) {
+   link[j].addEventListener('click', function () {
+      for (let i = 0; i < link.length; i++) {
+         link[i].classList.remove('active');
+      }
+      this.classList.add('active');
+   });
+};
+$(document).ready(function () {
+   $('.menu_toggle').click(function () {
+      $('.navbar').toggleClass('active');
+   })
+})
